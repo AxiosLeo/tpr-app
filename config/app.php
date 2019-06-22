3b54a87f1e82379f160de292587b5cf6ad439696
axios <axiosleo@foxmail.com>
2019-06-22 15:16:49 +0800

    update

--- config/app.php
+++ config/app.php
@@ -10,5 +10,5 @@
 return [
     "default_return_type"      => "html",
     "default_ajax_return_type" => "json",
-    "route_class_name"         => "{app_namespace}{module}\\controller\\{controller}"
+    "route_class_name"         => "{app_namespace}{module}\\controller\\{controller}", // 命名空间拼接格式自定义
 ];
--- config/routes.php
+++ config/routes.php
@@ -7,6 +7,7 @@
  * @datetime: 2019-03-19 14:26
  */
 
+// see : https://github.com/nikic/FastRoute
 return [
     "route_name" => [
         "rule"         => "/index/{id:\d+}[/{title}]",
