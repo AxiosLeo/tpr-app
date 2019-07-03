664dc786e7ec80e46129a1257080fcff37f5a6bf
wb-hx510875 <wb-hx510875@alibaba-inc.com>
2019-07-03 17:15:36 +0800

    update index.php file; remove set framework&vendor path

--- public/index.php
+++ public/index.php
@@ -14,8 +14,6 @@ use tpr\Path;
 
 require_once __DIR__ . '/../vendor/autoload.php';
 
-Path::framework(dirname(dirname(__DIR__)) . DIRECTORY_SEPARATOR);
 Path::root(dirname(__DIR__) . DIRECTORY_SEPARATOR);
-Path::vendor(Path::root() . "vendor" . DIRECTORY_SEPARATOR);
 
-App::mode(true)->run();
+App::run('App\\', true);
