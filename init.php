2d652cb04cc70662d88b823060a0f6f66763daaf
axios <axiosleo@foxmail.com>
2019-08-31 22:36:32 +0800

    update init.php; supported multi clients

--- init.php
+++ init.php
@@ -9,7 +9,9 @@ Path::root(__DIR__ . \DIRECTORY_SEPARATOR);
 Path::vendor(\dirname(__DIR__) . \DIRECTORY_SEPARATOR . 'vendor' . \DIRECTORY_SEPARATOR);
 
 if (defined("APP_NAMESPACE")) {
-    App::setAppOption("namespace", APP_NAMESPACE);
+    App::default()->setOption("namespace", APP_NAMESPACE);
 }
 
-App::run(true);
+App::default()
+    ->setOption('debug', true)
+    ->run();
