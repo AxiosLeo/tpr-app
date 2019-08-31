05dfe6a89acdd0c6028d1660fa9d6fcd9fcc37ee
axios <axiosleo@foxmail.com>
2019-08-31 22:44:38 +0800

    delete init.php

--- public/index.php
+++ public/index.php
@@ -2,7 +2,15 @@
 
 namespace tpr\index;
 
-define("APP_NAMESPACE", "app\\");
+use tpr\App;
+use tpr\Path;
 
-require_once __DIR__ . '/../init.php';
+require_once __DIR__ . '/../vendor/autoload.php';
 
+Path::root(realpath(dirname(__DIR__)) . \DIRECTORY_SEPARATOR);
+Path::vendor(Path::root() . 'vendor' . \DIRECTORY_SEPARATOR);
+
+App::default()
+    ->setOption("namespace", "app\\")
+    ->setOption('debug', true)
+    ->run();
